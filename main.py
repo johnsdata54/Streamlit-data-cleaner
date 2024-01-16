ad959994d5c6068349e46f42c4a07cc6daee64c8
Jsylves8 <jsylves8@students.kennesaw.edu>
2024-01-15 19:40:18 -0500

    Update 3.5

--- main.py
+++ main.py
@@ -1,39 +1,35 @@
 import streamlit as st
 import pandas as pd
-import random
+import os
 
 
 st.header("Data cleaner:")
 st.subheader("The intention of the program is to eliminate duplicates, empty values, and optionally remove columns")
+st.warning("This application currently only takes .CSV files")
 
-# sample data generated from chatgpt to test functionality
+file_dict = {1:"csv", 2:"sql", 3:"xlsx", 4:"xls"}
 
-# Sample data
-names = ['Alice', 'Bob', 'Charlie', 'David', 'David', 'Tatum', 'Kayla', 'Simone', 'Jack']
-ages = [random.randint(20, 30) for _ in range(len(names))]
-scores = [random.uniform(60, 100) for _ in range(len(names))]
-
-# Creating a DataFrame
-data = {'Name': names, 'Age': ages, 'Score': scores}
-
-df = pd.DataFrame(data)
+file_types = []
 
 uploaded_file = st.file_uploader("Upload a file")
 
-data = pd.read_csv(uploaded_file)
-
-df = pd.DataFrame(data)
 
 if uploaded_file is None:
-    pass
+
+    st.write("Please select a CSV file to proceed.")
 else:
+
+    file_name, file_ext = os.path.splitext(uploaded_file.name)
+    # Append the file extension to the list
+    file_types.append(file_ext)
+    # Print the list
+    
+
+    df = pd.read_csv(uploaded_file)
 
     dataframe_select = st.selectbox("Select preview amount for data frame", options = [5, 10, 15])
 
-    #saving data to dataframe
-    df = pd.DataFrame(data)
-
-    #this presents head of the data sheet
+        #this presents head of the data sheet
     st.write("Preview data head")
     head = df.head(dataframe_select)
     st.dataframe(head)
@@ -51,29 +47,33 @@ else:
 
     st.write(f"The number of duplicates: {num_duplicates}")
 
-   
-    if num_duplicates == 0:
-        decision = None
-    else:
+    dup_decision = st.checkbox("Yes?", key = 1)
 
-        decision = st.checkbox(f"Would you like to remove the duplicated entries from the data set")
-
-        backup_df_1 = df
-
-        if decision:
-            df = df.drop_duplicates()
+    if dup_decision:
+            
+        if num_duplicates == 1:
+            decision = None
         else:
-            df = backup_df_1
 
+            decision = st.checkbox(f"Would you like to remove the duplicated entries from the data set")
+
+            backup_df_1 = df
+
+            if decision:
+                df = df.drop_duplicates()
+            else:
+                df = backup_df_1
+    else:
+        pass
+    
     st.header("Would you like to remove Null/NaN values?")
-
+    
     #na_counts = df.groupby('Name')['value'].apply(lambda x: x.isna().sum())
 
     st.write("The number of null values per attribute: {} ")
 
     st.header("Would you like to remove columns?")
 
-    
 
     #All code under this comment -- functionality of what is written needs to be shown after a table of data has been uploaded
 
@@ -82,13 +82,33 @@ else:
     for columns in df.columns:
         list_columns.append(columns)
 
-    st.write("Please select columns to discard")
+    decision = st.checkbox(f"Yes?", key = 2)
 
-    columns_del = st.multiselect("Column names available", options = list_columns)
+    if decision:
+        st.write("Please select columns to discard")
 
-    st.write(columns_del)
+        columns_del = st.multiselect("Column names available", options = list_columns)
+
+        del_list_columns = []
+        
+        for i in range(len(columns_del)):
+
+            del_list_columns.append(columns_del[i])
+
+        if len(del_list_columns) > 0:
+
+            st.write(del_list_columns)
+
+    else:
+        pass
+
+
+    
     #All code under this comment -- functionality of what is written needs to be shown after a table of data has been uploaded
 
-    output = st.selectbox("Please select file output", ["SQL Database", "Excel Spreadsheet", "CSV" ])
+    output = st.selectbox("Please select file output", ["Database", "Excel Spreadsheet", "CSV" ])
 
+
+
+ 
     #st.download_button(label = f"Click to {output}", data = None)
